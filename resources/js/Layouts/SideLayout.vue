2f8386d5833f0b404658eab97fe540748b204281
FletcherHart <fletcherhart@gmail.com>
2021-07-05 15:37:45 -0400

    Add icons

--- resources/js/Layouts/SideLayout.vue
+++ resources/js/Layouts/SideLayout.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="md:w-1/3 border-l border-grey-100 py-10 px-5 ml-10 flex flex-col">
-    <footer class="flex flex-wrap gap-5 text-gray-600">
+    <div class="flex justify-center items-center">
+      <box-icon type='logo' name='twitter'></box-icon>
+      <box-icon type='logo' name='linkedin-square'></box-icon>
+      <box-icon type='logo' name='paypal'></box-icon>
+    </div>
+    <footer class="flex flex-wrap justify-center gap-5 text-gray-600">
       <inertia-link href="#">Placeholder 1</inertia-link>
       <inertia-link href="#">Placeholder 2</inertia-link>
       <inertia-link href="#">Placeholder 3</inertia-link>
