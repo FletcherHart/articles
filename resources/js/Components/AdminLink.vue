f57b69b975e6132f7cce96b6dee219b9f0b71a5e
FletcherHart <fletcherhart@gmail.com>
2021-08-09 10:36:42 -0400

    Create custom AdminLink component Custom links will be used for the admin panel, which will be separate from the links used on the rest of the site

---
+++ resources/js/Components/AdminLink.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <button :class="classes" class="w-full text-left" v-if="is == 'button'">
+      <slot></slot>
+    </button>
+
+    <inertia-link
+      :href="href"
+      class="flex items-center gap-5 text-white pl-3 pr-4 py-2 font-medium"
+      :class="classes"
+      v-else
+    >
+      <slot></slot>
+    </inertia-link>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["active", "href", "is"],
+
+  computed: {
+    classes() {
+      return this.active
+        ? "bg-indigo-500 hover:bg-indigo-700 border-l-4 border-transparent hover:border-white focus:outline-none transition"
+        : "hover:bg-gray-900 hover:text-blue-400 border-l-4 fill-current border-transparent hover:border-blue-400 transition"
+    },
+  },
+}
+</script>
