1f313251d964da8b48b989842a12ae4109710227
FletcherHart <fletcherhart@gmail.com>
2021-08-11 12:23:27 -0400

    Change how `name` is displayed, table width Name now displayed under email on smaller screens, which is a more appealing display. Table width changed from full to % for more consistent sizing across different screen sizes.

--- resources/js/Pages/EmailList.vue
+++ resources/js/Pages/EmailList.vue
@@ -1,16 +1,14 @@
 <template>
-  <app-layout>
-    <div class="flex flex-col w-full">
+  <admin-layout>
+    <div class="flex flex-col w-full px-2">
       <div
         v-if="$page.props.flash.error"
         class="w-full text-center text-lg text-red-700 py-10"
       >
         {{ $page.props.flash.error }}
       </div>
-      <div
-        class="flex md:flex-row flex-col-reverse gap-10 py-10 sm:px-5 w-full"
-      >
-        <div class="w-full">
+      <div class="flex flex-col-reverse gap-10 py-10 sm:px-5">
+        <div class="xl:w-11/12 lg:w-10/12 self-center">
           <div class="flex justify-between mb-5">
             <ButtonGray @click="page(this.pages.first)">
               <box-icon name="first-page"></box-icon> First
@@ -34,28 +32,35 @@
               sm:rounded-lg
             "
           >
-            <table class="w-full text-left divide-y divide-gray-200">
+            <table
+              class="w-full text-left divide-y divide-gray-200 table-fixed"
+            >
               <thead class="bg-gray-200 text-gray-700">
                 <tr>
-                  <th class="px-6 py-3">Subscribed</th>
-                  <th class="px-6 py-3">Name</th>
-                  <th class="w-2/4">Email Address</th>
-                  <th class="px-6 py-3">
+                  <th class="w-3/5">Email Address</th>
+                  <th class="w-1/5 px-6 py-3 hidden lg:block">Name</th>
+                  <th class="w-1/5 px-6 py-3">
                     <span class="sr-only">Delete</span>
                   </th>
                 </tr>
               </thead>
               <tbody class="bg-white divide-y divide-gray-200">
                 <tr v-for="address in addresses" :key="address.key">
-                  <td class="px-6 py-3">{{ address.subscribed }}</td>
-                  <td>{{ address.name }}</td>
-                  <td>{{ address.address }}</td>
-                  <td>
+                  <td class="">
+                    {{ address.address }}
+                    <div class="lg:hidden flex gap-1 text-sm">
+                      <p class="text-gray-700 inline">Name</p>
+                      {{ address.name }}
+                    </div>
+                  </td>
+                  <td class="hidden lg:table-cell">{{ address.name }}</td>
+                  <td class="float-right px-5">
                     <button
-                      class="p-3 bg-red-700 text-white"
+                      class="text-red-600 fill-current"
                       @click="destroy(address.address)"
+                      aria-label="Delete address"
                     >
-                      Delete
+                      <box-icon name="trash"></box-icon>
                     </button>
                   </td>
                 </tr>
@@ -76,18 +81,18 @@
         </div>
       </div>
     </div>
-  </app-layout>
+  </admin-layout>
 </template>
 
 <script>
-import AppLayout from "@/Layouts/AppLayout"
+import AdminLayout from "@/Layouts/AdminLayout"
 import { Inertia } from "@inertiajs/inertia"
 import { useForm } from "@inertiajs/inertia-vue3"
 import SubscribeEmail from "../Components/SubscribeEmail.vue"
 import ButtonGray from "../Components/ButtonGray.vue"
 
 export default {
-  components: { AppLayout, SubscribeEmail, ButtonGray },
+  components: { AdminLayout, SubscribeEmail, ButtonGray },
   props: {
     addresses: Array,
     pages: Array,
